c97e54ea9d370f7dc1ed32e4f11e775f4a3cc1b8
Xiaotian <58612496+Xwang11@users.noreply.github.com>
2023-05-10 11:41:39 -0400

    Added sound effects section, updated billiards

--- javascript/sounds.js
+++ javascript/sounds.js
@@ -1,11 +1,14 @@
 
 let on = new Array(9).fill(-1);
-let audios = [];
+let audioPaths = ["../audios/sheet.mp3", "../audios/howling.mp3", "../audios/crowd.mp3"];
+let audios = new Array(9).fill(new Audio("..."));
 
 function playEffect(index) {
     var isOn = on[index];
     var path = audios[index];
-    var audio = new Audio(path);
+    var audio = audios[index];
+    audio.
+    console.log(on[index]);
 
     if (isOn == -1) {
         audio.play();
@@ -13,4 +16,6 @@ function playEffect(index) {
     else {
         audio.pause();
     }
+
+    on[index] = isOn*-1;
 }
